2d6cf83b54973b028c232273f39f16178ce19ad0
Simone Stura (st148627) <stura.simone@educ.di.unito.it>
2019-11-06 00:08:30 +0100

    Me neither

---
+++ src/main/java/com/screesh/choosinghelper/ChoiceMade.java
@@ -0,0 +1,16 @@
+package com.screesh.choosinghelper;
+
+import com.screesh.model.Movie;
+
+public class ChoiceMade {
+    private Movie movie;
+    private boolean toExclude;
+    
+    public Movie getMovie() {
+        return movie;
+    }
+    
+    public boolean isToExclude() {
+        return toExclude;
+    }
+}
--- src/main/java/com/screesh/choosinghelper/ScheduleChooser.java
+++ src/main/java/com/screesh/choosinghelper/ScheduleChooser.java
@@ -2,38 +2,58 @@ package com.screesh.choosinghelper;
 
 import com.screesh.model.Movie;
 import com.screesh.model.Screening;
-import org.junit.Assert;
 
 import java.util.*;
 
-import static org.hamcrest.Matchers.*;
-
 public class ScheduleChooser {
     private ChoiceMaker choiceMaker;
-    private HashSet<Movie> allMovies;
-
+    private Set<Movie> allMovies;
+    
     public ScheduleChooser(HashSet<Movie> allMovies, ChoiceMaker choiceMaker) {
         this.allMovies = allMovies;
         this.choiceMaker = choiceMaker;
     }
     
     /**
-     *
      * @param solutions All the best solutions found
      * @return the collection of all the screenings which are included in a best solution after the choosing
      */
-    public SortedSet<Screening> chooseSolution(List<SortedSet<Screening>> solutions) {
-        Assert.assertNotNull(solutions);
-        Assert.assertTrue(solutions.size() > 0);
-        PriorityQueue<MovieCounting> exclusionCount = new PriorityQueue<>();
-        for(SortedSet<Screening> sol : solutions) {
+    public List<ChoiceMade> chooseSolution(List<SortedSet<Screening>> solutions) {
+        assert solutions != null;
+        assert solutions.size() > 0;
         
+        List<HashSet<Movie>> solutionsForMovies = buildSolutionsForMovies(solutions);
+    
+        PriorityQueue<MovieCounting> mostExcludedMovies = new PriorityQueue<>(allMovies.size());
+        
+        for (Movie movie : allMovies) {
+            MovieCounting exclusionsCounter = new MovieCounting(movie);
+            for (HashSet<Movie> moviesInSolution : solutionsForMovies) {
+                if(!moviesInSolution.contains(movie))
+                    exclusionsCounter.counting++;
+            }
+            mostExcludedMovies.add(exclusionsCounter);
         }
-
-        return null;
+        
+        List<ChoiceMade> choices = new ArrayList<>();
+        
+        return choices;
     }
     
-    private class MovieCounting implements Comparable<MovieCounting> {
+    private List<HashSet<Movie>> buildSolutionsForMovies(List<SortedSet<Screening>> solutions) {
+        ArrayList<HashSet<Movie>> solutionsForMovies = new ArrayList<>(solutions.size());
+        
+        for (SortedSet<Screening> sol : solutions) {
+            HashSet<Movie> moviesInSolution = new HashSet<>();
+            for (Screening screen : sol)
+                moviesInSolution.add(screen.getScreened());
+            solutionsForMovies.add(moviesInSolution);
+        }
+        
+        return solutionsForMovies;
+    }
+    
+    private static class MovieCounting implements Comparable<MovieCounting> {
         Movie value;
         int counting;
         
@@ -43,7 +63,7 @@ public class ScheduleChooser {
         }
         
         public int compareTo(MovieCounting o) {
-            return Integer.compare(o.counting, this.counting);
+            return Integer.compare(this.counting, o.counting);
         }
     }
 }
